c1b08b618950ec393f63bb57a8547b5de030c2db
Lazaro Morales <moraleslazaro@gmail.com>
2022-08-26 22:22:25 -0400

    Used wrong boot directory

--- install.sh
+++ install.sh
@@ -106,7 +106,7 @@ passwd
 
 # Configure boot loader
 printf "Setting up ${BLD}GRUB${RST}...\n"
-grub-install --target=x86_64-efi --efi-directory=/mnt/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Reboot
